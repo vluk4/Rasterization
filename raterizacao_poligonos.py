f77bc00a1e190bf12805d7e17b940b867a1e1e95
Victor Luca <vluca002@gmail.com>
2023-10-01 23:30:52 -0300

    Ajustando algoritmo

--- raterizacao_poligonos.py
+++ raterizacao_poligonos.py
@@ -17,6 +17,12 @@ import numpy as np
 # -0.5,0; -0.25,0.433; 0.25,0.433; 0.5,0; 0.25,-0.433; -0.25,-0.433
 # -0.25,0.125; -0.125,0.375; 0.125,0.375; 0.25,0.125; 0.125,-0.125; -0.125,-0.125
 
+# Retas:
+# -1,0;1,0
+# 0,-1;0,1
+# -1,-1;1,1
+# -1,1;1,-1
+# 0.75,0.5;-0.75,-1
 # Lista de poligonos
 poligonos = []
 
@@ -25,8 +31,8 @@ def normalizar_coordenadas(pontos, res_x, res_y):
     pontos_normalizados = []
     for ponto in pontos:
         # Transforma a coordenada x e y no intervalo [-1, 1] para o espaço de pixel
-        x = (ponto[0] + 1) / 2 * (res_x - 1)
-        y = (ponto[1] + 1) / 2 * (res_y - 1)
+        x = (ponto[0] + 1) / 2 * res_x
+        y = (ponto[1] + 1) / 2 * res_y
         pontos_normalizados.append((x, y))
     return pontos_normalizados
 
@@ -39,11 +45,13 @@ def rasterizar_reta(x1, y1, x2, y2, res_x, res_y):
     dx = x2 - x1
     dy = y2 - y1
 
+    m = dy / dx if dx != 0 else 0
+
     # Verifica se a reta é mais horizontal do que vertical
     if abs(dx) > abs(dy):
         if x1 > x2:
             x1, x2, y1, y2 = x2, x1, y2, y1
-        m = dy / dx
+
         b = y1 - m * x1
         x, y = x1, y1
 
@@ -58,7 +66,7 @@ def rasterizar_reta(x1, y1, x2, y2, res_x, res_y):
     else:
         if y1 > y2:
             x1, x2, y1, y2 = x2, x1, y2, y1
-        m = dx / dy
+
         b = x1 - m * y1
         x, y = x1, y1
 
@@ -77,19 +85,14 @@ def rasteriza_poligno(imagem):
     pontos_internos = []
     # Iterar sobre cada pixel na imagem
     for y in range(res_y):
-        is_aresta = False
         count = 0
         ponto_aux = []
         for x in range(res_x):
             if imagem[y, x] == 1:
                 if y < res_y and (x + 1) < res_x and imagem[y, x + 1] == 1:
-                    is_aresta = True
-                    count = 0
+                    pass
                 else:
-                    if is_aresta:
-                        is_aresta = False
-                    else:
-                        count += 1
+                    count += 1
 
             if 0 < count and count % 2 == 1:
                 ponto_aux.append([y, x])
@@ -116,24 +119,21 @@ def mostrar_poligonos():
     imagem_final = np.zeros((res_y, res_x))
 
     for poligono in poligonos:
+        poligono_rasterizado = np.zeros((res_y, res_x))
         pontos = [tuple(map(float, ponto.split(','))) for ponto in poligono]
         pontos_normalizados = normalizar_coordenadas(pontos, res_x, res_y)
         for i, ponto1 in enumerate(pontos_normalizados):
             ponto2 = pontos_normalizados[i - 1]  # O ponto anterior na lista
-            imagem_rasterizada_retas = rasterizar_reta(ponto1[0], ponto1[1], ponto2[0], ponto2[1], res_x, res_y)
+            reta_rasterizada = rasterizar_reta(ponto1[0], ponto1[1], ponto2[0], ponto2[1], res_x, res_y)
+            poligono_rasterizado = np.maximum(poligono_rasterizado, reta_rasterizada)
 
-            imagem_final = np.maximum(imagem_final, imagem_rasterizada_retas)
-
-        imagem_reasterizada_poligonos = rasteriza_poligno(imagem_final)
+        imagem_reasterizada_poligonos = rasteriza_poligno(poligono_rasterizado)
         imagem_final = np.maximum(imagem_final, imagem_reasterizada_poligonos)
-
-    print(np.array_str(imagem_final))
 
     plt.title(f'Resolução: {res_x}x{res_y}')
     plt.imshow(imagem_final, cmap='gray', origin='lower')
     plt.xticks(range(0, res_x, res_x // 10))
     plt.yticks(range(0, res_y, res_y // 10))
-    plt.grid()
     plt.show()
 
 
