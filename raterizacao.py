d30f2c7f6b1bf3ec96a1aa82ed6f345522a00c9d
Victor Luca <vluca002@gmail.com>
2023-10-09 20:26:34 -0300

    Adicionando rasterização de curvas

--- raterizacao.py
+++ raterizacao.py
@@ -1,38 +1,36 @@
 import sys
 from tkinter import Tk, Label, Entry, Button, Text, Scrollbar, END
-
+import tkinter as tk
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Exemplos:
 # Triângulos equiláteros:
 # -0.9, -0.25; -0.5,-0.25; -0.7, 0.25
-# -0.25,0; 0.25,0; 0,0.433
-
-# Quadrados:
-#  -0.25,-0.25; 0.25,-0.25; 0.25,0.25; -0.25,0.25
-# -0.5,-0.5; 0.5,-0.5; 0.5,0.5; -0.5,0.5
-
-# Hexágonos:
-# -0.5,0; -0.25,0.433; 0.25,0.433; 0.5,0; 0.25,-0.433; -0.25,-0.433
+# Quadrado:
+# 0.5,0; 0.9,0; 0.9,0.4; 0.5,0.4
+# Hexágono:
 # -0.25,0.125; -0.125,0.375; 0.125,0.375; 0.25,0.125; 0.125,-0.125; -0.125,-0.125
-
 # Retas:
 # -1,0;1,0
 # 0,-1;0,1
 # -1,-1;1,1
 # -1,1;1,-1
 # 0.75,0.5;-0.75,-1
-# Lista de poligonos
+# Curvas
+# -0.5, 0.5; -0.2, 0.5; 0, 0.2; 0, -0.2; 5
+# 0.2, -0.2; 0.5, -0.5; 0.2, 0; -0.2, 0; 5
+
 poligonos = []
+curvas_hermite = []
 
 
 def normalizar_coordenadas(pontos, res_x, res_y):
     pontos_normalizados = []
     for ponto in pontos:
         # Transforma a coordenada x e y no intervalo [-1, 1] para o espaço de pixel
-        x = (ponto[0] + 1) / 2 * res_x
-        y = (ponto[1] + 1) / 2 * res_y
+        x = (ponto[0] + 1) / 2 * res_x - 1
+        y = (ponto[1] + 1) / 2 * res_y - 1
         pontos_normalizados.append((x, y))
     return pontos_normalizados
 
@@ -102,12 +100,41 @@ def rasteriza_poligno(imagem):
     return imagem
 
 
+def rasterizar_curva_hermite(p1, p2, t1, t2, res_x, res_y, num_points):
+    imagem = np.zeros((res_y, res_x))
+    points = []
+
+    for t in range(num_points):
+        t_normalized = t / (num_points - 1)
+        h1 = 2 * t_normalized ** 3 - 3 * t_normalized ** 2 + 1
+        h2 = -2 * t_normalized ** 3 + 3 * t_normalized ** 2
+        h3 = t_normalized ** 3 - 2 * t_normalized ** 2 + t_normalized
+        h4 = t_normalized ** 3 - t_normalized ** 2
+        x = h1 * p1[0] + h2 * p2[0] + h3 * t1[0] + h4 * t2[0]
+        y = h1 * p1[1] + h2 * p2[1] + h3 * t1[1] + h4 * t2[1]
+        points.append((int(x), int(y)))
+
+    for i in range(len(points) - 1):
+        x1, y1 = points[i]
+        x2, y2 = points[i + 1]
+        imagem = np.maximum(imagem, rasterizar_reta(x1, y1, x2, y2, res_x, res_y))
+    return imagem
+
+
 def adicionar_poligono():
     pontos_str = entrada_pontos.get()
     entrada_pontos.delete(0, END)
     poligonos_str = pontos_str.split(';')
     poligonos.append(poligonos_str)
-    texto_poligonos.insert("end", f"{len(poligonos)}. {pontos_str}\n")
+    texto_pontos.insert("end", f"{len(poligonos)}. {pontos_str}\n")
+
+
+def adicionar_curva_hermite():
+    pontos_str = entrada_curva_hermite.get()
+    entrada_curva_hermite.delete(0, END)
+    curva_hermite_str = pontos_str.split(';')
+    curvas_hermite.append(curva_hermite_str)
+    texto_pontos.insert("end", f"{len(curvas_hermite)}. {pontos_str}\n")
 
 
 def mostrar_poligonos():
@@ -126,6 +153,17 @@ def mostrar_poligonos():
         imagem_reasterizada_poligonos = rasteriza_poligno(poligono_rasterizado)
         imagem_final = np.maximum(imagem_final, imagem_reasterizada_poligonos)
 
+    for curva_hermite in curvas_hermite:
+        pontos = [tuple(map(float, ponto.split(','))) for ponto in curva_hermite[:-1]]
+        pontos_normalizados = normalizar_coordenadas(pontos, res_x, res_y)
+        p1 = pontos_normalizados[0]
+        p2 = pontos_normalizados[1]
+        t1 = pontos_normalizados[2]
+        t2 = pontos_normalizados[3]
+        pontos_qnt = curva_hermite[-1]
+        curva_hermite_rasterizada = rasterizar_curva_hermite(p1, p2, t1, t2, res_x, res_y, int(pontos_qnt))
+        imagem_final = np.maximum(imagem_final, curva_hermite_rasterizada)
+
     plt.title(f'Resolução: {res_x}x{res_y}')
     plt.imshow(imagem_final, cmap='gray', origin='lower')
     plt.xticks(range(0, res_x, res_x // 10))
@@ -134,28 +172,36 @@ def mostrar_poligonos():
 
 
 root = Tk()
-root.title("Rasterização de Polígonos")
+root.title("Rasterização")
 
-Label(root, text="Pontos do Polígono (x1,y1;x2,y2;...):").grid(row=0, column=0, sticky="w")
-entrada_pontos = Entry(root, width=30)
-entrada_pontos.grid(row=0, column=1)
+# Retas e polígonos
+Label(root, text="Pontos da reta ou polígono (x1,y1;x2,y2;...):").grid(row=0, column=0, sticky="w", pady=10)
+entrada_pontos = Entry(root)
+entrada_pontos.grid(row=0, column=1, sticky=tk.W + tk.E)
 
-botao_adicionar = Button(root, text="Adicionar Polígono", command=adicionar_poligono)
-botao_adicionar.grid(row=1, column=0, columnspan=2)
+botao_adicionar = Button(root, text="Adicionar Pontos", command=adicionar_poligono)
+botao_adicionar.grid(row=1, column=0, columnspan=2, ipady=5)
 
-Label(root, text="Polígonos adicionados:").grid(row=2, column=0, sticky="w")
-texto_poligonos = Text(root, width=35, height=10)
-texto_poligonos.grid(row=3, column=0, columnspan=2)
+Label(root, text="Curva de Hermite (P1x,P1y;p2x,p2y;T1x,T1y;T2x,T2y;qnt):").grid(row=2, column=0, sticky="w", pady=10)
+entrada_curva_hermite = Entry(root, width=40)
+entrada_curva_hermite.grid(row=2, column=1)
 
-scrollbar = Scrollbar(root, command=texto_poligonos.yview)
-scrollbar.grid(row=3, column=2, sticky="ns")
-texto_poligonos.config(yscrollcommand=scrollbar.set)
+botao_adicionar = Button(root, text="Adicionar Pontos", command=adicionar_curva_hermite)
+botao_adicionar.grid(row=3, column=0, columnspan=2, ipady=5)
 
-Label(root, text="Resolução (largura x altura):").grid(row=4, column=0, sticky="w")
+Label(root, text="Pontos adicionados:").grid(row=4, column=0, sticky="w")
+texto_pontos = Text(root, width=35, height=10)
+texto_pontos.grid(row=4, column=0, columnspan=2, pady=10)
+
+scrollbar = Scrollbar(root, command=texto_pontos.yview)
+scrollbar.grid(row=4, column=3, sticky="ns")
+texto_pontos.config(yscrollcommand=scrollbar.set)
+
+Label(root, text="Resolução (largura x altura):").grid(row=6, column=0, sticky="w")
 entrada_resolucao = Entry(root, width=30)
-entrada_resolucao.grid(row=4, column=1)
+entrada_resolucao.grid(row=6, column=1, sticky=tk.W + tk.E)
 
-botao_rasterizar = Button(root, text="Rasterizar Polígonos", command=mostrar_poligonos)
-botao_rasterizar.grid(row=5, column=0, columnspan=2)
+botao_rasterizar = Button(root, text="Rasterizar", command=mostrar_poligonos)
+botao_rasterizar.grid(row=7, column=0, columnspan=2, ipady=5, pady=10)
 
 root.mainloop()
